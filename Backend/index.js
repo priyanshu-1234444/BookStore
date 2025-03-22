3b08fda61e7bbd6c3f829ed52941cb205783dc02
priyanshu-1234444 <153397191+priyanshu-1234444@users.noreply.github.com>
2025-03-22 23:45:15 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import bookRoute from "./route/book.route.js"
+import userRoute from "./route/user.route.js";
+
 const app = express();
 app.use(cors());
 dotenv.config();
@@ -19,6 +21,7 @@ mongoose.connect(process.env.MongoDBURI, {
     });
 // defining routes
 app.use("/book",bookRoute)
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
